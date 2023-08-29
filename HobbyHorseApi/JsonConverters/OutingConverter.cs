8c2562eaf51c70cfdd150ae9d93c30f41430b271
Alexandru Pop <alex99_pop@yahoo.com>
2023-08-29 18:59:29 +0300

    fix

--- HobbyHorseApi/JsonConverters/OutingConverter.cs
+++ HobbyHorseApi/JsonConverters/OutingConverter.cs
@@ -146,16 +146,18 @@ namespace HobbyHorseApi.JsonConverters
                 writer.WriteNumber("closingHour", parkTrail.ClosingHour);
 
                 //location object should not serialized here?
-                //writer.WriteStartObject("location");
-                //    writer.WriteString("id", parkTrail.Location.Id);
-                //    writer.WriteString("name", parkTrail.Location.Name);
-                //    writer.WriteString("imageUrl", parkTrail.Location.ImageUrl);
-                //    writer.WriteNumber("lat", parkTrail.Location.Lat);
-                //    writer.WriteNumber("long", parkTrail.Location.Long);
-                //    writer.WriteEndObject();
+                if(parkTrail.Location != null)
+                {
+                    writer.WriteStartObject("location");
+                    writer.WriteString("id", parkTrail.Location.Id);
+                    writer.WriteString("name", parkTrail.Location.Name);
+                    writer.WriteString("imageUrl", parkTrail.Location.ImageUrl);
+                    writer.WriteNumber("lat", parkTrail.Location.Lat);
+                    writer.WriteNumber("long", parkTrail.Location.Long);
+                    writer.WriteEndObject();
+                }
 
-                
-                //writer.WriteEndObject();
+                writer.WriteEndObject();
             }
             else
             {
